ff865cd1dbb7d60ee82d0bd0b8191f3073b8de4f
meghraj jagtap <itsmemeghraj@gmail.com>
2020-08-20 02:15:37 +0530

    find project

--- ppmtool/src/main/java/com/example/demo/services/ProjectService.java
+++ ppmtool/src/main/java/com/example/demo/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import com.example.demo.domain.Project;
+import com.example.demo.exceptions.ProjectIdException;
 import com.example.demo.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,19 @@ public class ProjectService {
 
     public Project saveOrUpdate(Project project){
 
-        return projectRepository.save(project);
+            try{
+                project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+                return projectRepository.save(project);
+
+            }catch(Exception e){
+
+                throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+            }
+
     }
 
+    public Project findProjectByIdentifier(String projectId){
+        return projectRepository.findByProjectIdentifier(projectId);
+    }
 
 }
